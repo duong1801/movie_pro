ff156f4e5ad4d0011b1c67f203d9e722b0838aae
duong1801 <duongx1801@gmail.com>
2023-08-29 13:45:51 +0700

    update code

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,12 +4,14 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Pỏnhub VN</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
+
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
         <meta property="og:title"              content="Pỏn hub VN" />
+        <meta property="og:type" content="article" />
         <meta property="og:description"        content="Thay đổi nền công nghiệp không khói" />
         <!-- Styles -->
         <style>
