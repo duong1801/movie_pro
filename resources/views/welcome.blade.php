3b4a5c539d29e56934220c69708de6e7020f746c
duong1801 <duongx1801@gmail.com>
2023-08-29 13:35:45 +0700

    update code

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -35,7 +35,7 @@
 
             <div class="max-w-7xl mx-auto p-6 lg:p-8">
                 <div class="flex justify-center">
-                    <h1 class="text-7xl">Pỏn hub VN</h1>
+                    <h1 class="text-xl">Pỏn hub VN</h1>
                 </div>
 
                 <div class="mt-16">
